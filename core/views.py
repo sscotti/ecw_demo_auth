2a8a4366bbcb7ec3a3f22973b792d162257b6d1c
adamyala <adamkyala@gmail.com>
2024-12-06 16:23:32 -0700

    new code challenge logic

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 import base64
 import hashlib
 import os
+import secrets
 from uuid import uuid4
 
 import requests
@@ -23,8 +24,11 @@ def launch(request):
     # create a uuid to id the state. pretend we save it for later
     state = str(uuid4())
 
-    code_verifier = base64.urlsafe_b64encode(os.urandom(64)).decode("utf-8")
-    code_challenge_sha = hashlib.sha256(code_verifier.encode("utf-8")).digest()
+    # b"a33e1e9be0725bf6ab7705f022ea80543d1718b89a0bea35f95e7375bb216e51"
+    code_verifier = secrets.token_hex(32).encode("utf-8")
+    # b"4da689c0e38bb59f5ba1c452a3d2206c4d74a6473c491f6f2d759ae6fe544d97"
+    code_challenge_sha = hashlib.sha256(code_verifier).hexdigest().encode("utf-8")
+    # "NGRhNjg5YzBlMzhiYjU5ZjViYTFjNDUyYTNkMjIwNmM0ZDc0YTY0NzNjNDkxZjZmMmQ3NTlhZTZmZTU0NGQ5Nw"
     code_challenge = base64.urlsafe_b64encode(code_challenge_sha).decode("utf-8")
     print(f"{code_verifier=} {code_challenge_sha=} {code_challenge=}")
 
